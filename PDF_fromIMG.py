299672bee8091396ebc43b10b691246b169b271b
Nobuho <novufo13@gmail.com>
2020-05-05 08:36:33 +0900

    comit

--- PDF_fromIMG.py
+++ PDF_fromIMG.py
@@ -1,3 +1,4 @@
+from PIL import Image
 import img2pdf
 import os
 from glob import glob
@@ -12,10 +13,19 @@ ext = ".jpg"
 
 path += "\\**\\"
 
+
 for p in [i for i in glob(path)]:
     search_path = p + "*" + ext
     folder_name = os.path.basename(p.rstrip(os.sep))
     print(folder_name)
+    for i in glob(search_path):
+        with Image.open(i) as src:
+            data = src.getdata()
+            mode = src.mode
+            size = src.size
+        with Image.new(mode, size) as dst:
+            dst.putdata(data)
+            dst.save(i)
     if len([i for i in glob(search_path)]) == 0:
         continue
     else:
